89000c89d812259a7336b23b9d269b6cc9790c99
Festus Kwafo <68119922+Festus-Kwafo@users.noreply.github.com>
2022-05-30 22:56:28 +0000

    database connect postgresql

--- accounts/models.py
+++ accounts/models.py
@@ -26,8 +26,6 @@ class Locum(models.Model):
     gender = models.CharField(max_length=20, null=True, choices=GENDER)
     about_me = models.TextField()
     service = models.CharField(max_length=255, null=True)
-    id_file = models.FileField(upload_to='validation/%Y/%m/%d/', null=True)
-    license_file = models.FileField(upload_to='validation/%Y/%m/%d/', null=True)
     cv_file = models.FileField(upload_to='validation/%Y/%m/%d/', null=True)
 
     def __str__(self):
@@ -39,7 +37,6 @@ class Institution(models.Model):
     location = models.CharField(max_length=255, null=True)
     name_organisation = models.CharField(max_length=255, null=True)
     service = models.CharField(max_length=255, null=True)
-    id_file = models.FileField(upload_to='validation/%Y/%m/%d/', null=True)
     license_file = models.FileField(upload_to='validation/%Y/%m/%d/', null=True)
 
 
